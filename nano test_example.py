01b1119d32e26245c000300a255cd9e70d15b862
Triploed <kolyavid15@gmail.com>
2026-04-17 13:36:35 +0400

    Первый тест

---
+++ nano test_example.py
@@ -0,0 +1,5 @@
+def test_addition():
+    assert 3 + 4 == 7
+
+def test_upper():
+    assert "hello".upper() == "HELLO"
---
+++ test_sample.py
@@ -0,0 +1,5 @@
+def test_sample():
+    assert 2 + 2 == 4
+
+def test_another():
+    assert "hello".upper() == "HELLO"
